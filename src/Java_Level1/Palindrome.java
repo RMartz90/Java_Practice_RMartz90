4f4d7468f8f6b25fad47790343e4fc37af3403a9
Usuario <rmc9009@gmail.com>
2025-07-14 17:16:12 -0600

    Translating Java exercises to English

---
+++ src/Java_Level1/Palindrome.java
@@ -0,0 +1,26 @@
+package Java_Level1;
+
+public class Palindrome {
+    public static void main (String[] args){
+
+        String word = "No lemon, no melon";
+        word = word.replace(" ", "");
+
+        if (Palindrome(word)){
+            System.out.println(word + ": is a palíndrome.");
+        } else {
+            System.out.println(word + ": is not a palindrome.");
+        }
+    }
+    
+    public static boolean Palindrome(String word) {
+        word = word.toLowerCase();
+
+        for (int i = 0, j = word.length()/2 - 1; i <= j; i++, j--) {
+            if (word.charAt(i) == word.charAt(j))
+                return true;}
+
+        return false;
+
+    }
+}
---
+++ src/Java_Level1/listPalindrome.java
@@ -0,0 +1,26 @@
+package Java_Level1;
+
+public class listPalindrome {
+    public static void main(String[] args) {
+        String[] words = {"raceCar", "house", "rAdar", "java", "level", "Surprise"};
+
+        for (String word : words) {
+            if (isaPalindrome(word)){
+                System.out.println(word + ": is a palíndrome.");
+            } else {
+                System.out.println(word + ": is not a palindrome.");
+            }
+        }
+    }
+
+    public static boolean isaPalindrome(String word) {
+        word = word.toLowerCase();
+
+        for (int i = 0, j = word.length() - 1; i <= j; i++, j--) {
+
+            if (word.charAt(i) != word.charAt(j))
+                return false;}
+
+        return true;
+    }
+}
