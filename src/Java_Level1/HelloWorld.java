bb7ad390fdb42ae695d6d55053cff4ea7d368fce
Usuario <rmc9009@gmail.com>
2025-07-15 00:44:33 -0600

    Adding a basic Hello world and Moving Palindrome exercises to Level 2

---
+++ src/Java_Level1/HelloWorld.java
@@ -0,0 +1,9 @@
+package Java_Level1;
+
+public class HelloWorld {
+
+    public static void main (String[] args){
+            System.out.println("Hello My Baby! Hello My Honey!, Hello My RagTime Gal!");
+            System.out.println("Sorry.. I meant to say.. Hello World!");
+        }  
+}
--- src/Java_Level2/Palindrome.java
+++ src/Java_Level2/Palindrome.java
@@ -1,4 +1,4 @@
-package Java_Level1;
+package Java_Level2;
 
 public class Palindrome {
     public static void main (String[] args){
